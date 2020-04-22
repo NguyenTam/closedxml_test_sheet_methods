45e251e100eb03dd502131fe9e01194c9519438a
Tam Nguyen <tam.nguyen@fi.abb.com>
2020-04-22 12:18:41 +0300

    reproduction

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace closedxml_empty_sheet_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ClosedXML.Excel.XLWorkbook workbook = null;
+            workbook = new ClosedXML.Excel.XLWorkbook("../../WaterPumpSystem.xlsx");
+
+            foreach (var sheet in workbook.Worksheets)
+            {
+                if (sheet.IsEmpty())
+                {
+                    System.Console.WriteLine(sheet.Name + " is empty");
+                }
+                else
+                {
+                    System.Console.WriteLine(sheet.Name + " is NOT empty");
+                }
+            }
+        }
+    }
+}
---
+++ README_OUTPUT.txt
@@ -0,0 +1,8 @@
+#My debug run output is:
+----------------------
+Emptysheet is empty
+Types is empty
+Tanks is empty
+Pumps is empty
+Pipes is NOT empty
+------------------
